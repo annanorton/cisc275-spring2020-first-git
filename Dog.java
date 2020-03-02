25b3208ebd705b3e3c566b439d5a20f71bf1f3ec
annanorton <61671970+annanorton@users.noreply.github.com>
2020-03-01 21:43:05 -0500

    update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Dog extends Animal implements Comparable<Dog>{
+public class Dog extends Animal {
 
   public Dog(String name, int legs) {
     super(name, legs);
