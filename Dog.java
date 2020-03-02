9040b2ccc8c5d9ecab68ae7c6be29a412cc282c1
annanorton <61671970+annanorton@users.noreply.github.com>
2020-03-01 21:34:52 -0500

    deleted main in Dog.java

--- Dog.java
+++ Dog.java
@@ -9,20 +9,4 @@ public class Dog extends Animal implements Comparable<Dog>{
   public String toString() {
     return "name: " + name + " legs: " + legs;
   }
-
-  public static void main(String[] args) {
-    ArrayList<Dog> pack = new ArrayList<Dog>();
-    Dog d1 = new Dog("Sydney", 4);
-    Dog d2 = new Dog("Chloe", 4);
-    Dog d3 = new Dog("Sydney", 3);
-    pack.add(d1);
-    pack.add(d2);
-    pack.add(d3);
-    System.out.println(pack);
-    System.out.println(Collections.sort(pack));
-  }
-
-  public int compareTo() { //natural ordering
-    return 0;
-  }
 }
